d836be409657aac9aa30f67eb98d3bfefee41d1d
aihuachuan <501320365@qq.com>
2019-03-24 15:00:44 +0400

    update:兼容处理

--- UI/eventsearch.cpp
+++ UI/eventsearch.cpp
@@ -238,7 +238,8 @@ EventSearch::EventSearch( WidgetI *parent):
 
     QVector<std::tuple<QString,QString,QString,QColor>> subTypeAlarmVec;
     subTypeAlarmVec << std::make_tuple(tr("smsr_alarm_black"),tr("Blacklist"),tr("100010001008"),Qt::transparent)
-                    << std::make_tuple(tr("smsr_alarm_vip"),tr("VIP"),tr("100010001007"),Qt::transparent);
+                    << std::make_tuple(tr("smsr_alarm_vip"),tr("VIP"),tr("100010001007"),Qt::transparent)
+                    << std::make_tuple(tr("smsr_alarm_face"),tr("Staff"),tr("100010001005"),Qt::transparent);
     for(int i = 0; i < subTypeAlarmVec.count(); i++){
         QString personTypeNo = std::get<2>(subTypeAlarmVec.at(i));
         waringColorMap_.insert(std::get<1>(subTypeAlarmVec.at(i)),std::get<3>(subTypeAlarmVec.at(i)));
